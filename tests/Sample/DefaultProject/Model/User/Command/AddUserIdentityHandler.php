2bec0ffc0fb41e4c0812fbd9cbda7722d2b4ea83
Bas Kamer <bas@bushbaby.nl>
2018-11-02 14:29:59 +0100

    apply prooph cs-fixer

--- tests/Sample/DefaultProject/Model/User/Command/AddUserIdentityHandler.php
+++ tests/Sample/DefaultProject/Model/User/Command/AddUserIdentityHandler.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the prooph/message-flow-analyzer.
+ * This file is part of prooph/message-flow-analyzer.
  * (c) 2017-2018 prooph software GmbH <contact@prooph.de>
  * (c) 2017-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
